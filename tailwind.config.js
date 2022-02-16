2b74eb0a434901b665eb65cfedf65fa94e2814ea
Ferruh <ferruhcihan20@gmail.com>
2022-02-16 14:43:11 +0300

    feat:start project structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   // Uncomment the line below to enable the experimental Just-in-Time ("JIT") mode.
   // https://tailwindcss.com/docs/just-in-time-mode
-  // mode: "jit",
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: (theme) => ({
+        "brand-color": "#5d3ebc",
+      }),
+    },
   },
   variants: {},
   plugins: [],
