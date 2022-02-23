06e009edb2045cec96a08ba9ad6ebafbd11275eb
Ferruh <ferruhcihan20@gmail.com>
2022-02-23 21:53:26 +0300

    feat:add hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import Slider from "react-slick";
+import ReactFlagsSelect from "react-flags-select";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  const [selected, setSelected] = useState("TR");
+
+  const flags = {
+    US: "+1",
+    NL: "+31",
+    FR: "+33",
+    IT: "+39",
+    GB: "+44",
+    DE: "+49",
+    TR: "+90",
+  };
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  };
+
+  return (
+    <div className="relative h-[500px] before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
+      <Slider {...settings}>
+        <div>
+          <img
+            className="w-full h-[500px] object-cover"
+            src="https://getir.com/_next/static/images/getir-mainpage-4-1751ad2d8fb42a88742d6751938da7e7.jpg"
+            alt="img-1"
+          />
+        </div>
+        <div>
+          <img
+            className="w-full h-[500px] object-cover"
+            src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg"
+            alt="img-1"
+          />
+        </div>
+      </Slider>
+      <div className="container flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20">
+        <div>
+          <img
+            src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg"
+            alt="getir-bi-mutluluk"
+          />
+          <h3 className="mt-8 text-4xl font-semibold text-white">
+            Dakikalar içinde <br /> kapınızda <br />
+          </h3>
+        </div>
+        <div className="w-[400px] rounded-lg bg-gray-50 p-6">
+          <h4 className="text-primary-brand-color text-center font-semibold mb-4">
+            Giriş yap veya kayıt ol
+          </h4>
+          <div className="flex">
+            {/* @ts-ignore */}
+            <ReactFlagsSelect
+              countries={Object.keys(flags)}
+              customLabels={flags}
+              selected={selected}
+              onSelect={(code) => setSelected(code)}
+              className="flag-select"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
