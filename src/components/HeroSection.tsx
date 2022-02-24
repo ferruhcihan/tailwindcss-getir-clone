c6ebd73fb2df4e2c0cd1180d68efaa5fabbf2e13
Ferruh <ferruhcihan20@gmail.com>
2022-02-24 22:30:24 +0300

    feat:update hero section & complete form

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Slider from "react-slick";
 import ReactFlagsSelect from "react-flags-select";
+import { FaFacebook } from "react-icons/fa";
 
 const HeroSection = () => {
   const [selected, setSelected] = useState("TR");
@@ -57,24 +58,34 @@ const HeroSection = () => {
           <h4 className="text-primary-brand-color text-center font-semibold mb-4">
             Giriş yap veya kayıt ol
           </h4>
-          <div className="flex gap-x-2">
-            {/* @ts-ignore */}
-            <ReactFlagsSelect
-              countries={Object.keys(flags)}
-              customLabels={flags}
-              selected={selected}
-              onSelect={(code) => setSelected(code)}
-              className="flag-select"
-            />
-            <label className="relative flex-1 block">
-              <input
-                required
-                className="h-full rounded px-4 pt-2 hover:border-primary-brand-color focus:border-primary-brand-color transition-colors outline-none text-sm w-full border-2 border-gray-200 peer"
+          <div className="grid gap-y-3">
+            <div className="flex gap-x-2">
+              {/* @ts-ignore */}
+              <ReactFlagsSelect
+                countries={Object.keys(flags)}
+                customLabels={flags}
+                selected={selected}
+                onSelect={(code) => setSelected(code)}
+                className="flag-select"
               />
-              <span className="absolute top-0 left-0 h-full flex items-center px-4 text-sm text-gray-500 transition-all group-focus-within:h-7 group-focus-within:text-xs group-focus-within:text-primary-brand-color peer-valid:h-7 peer-valid:text-xs peer-valid:text-primary-brand-color">
-                Telefon Numarası
-              </span>
-            </label>
+              <label className="relative flex-1 group block cursor-pointer">
+                <input
+                  required
+                  className="h-full rounded px-4 pt-2 group-hover:border-primary-brand-color focus:border-primary-brand-color transition-colors outline-none text-sm w-full border-2 border-gray-200 peer"
+                />
+                <span className="absolute top-0 left-0 h-full flex items-center px-4 text-sm text-gray-500 transition-all group-focus-within:h-7 group-focus-within:text-xs group-focus-within:text-primary-brand-color peer-valid:h-7 peer-valid:text-xs peer-valid:text-primary-brand-color">
+                  Telefon Numarası
+                </span>
+              </label>
+            </div>
+            <button className="bg-brand-yellow text-primary-brand-color transition-colors hover:bg-primary-brand-color hover:text-brand-yellow h-12 flex items-center justify-center rounded-md w-full text-sm font-semibold text-primary-brand-color">
+              Telefon numarası ile devam et
+            </button>
+            <hr className="h-[1px] bg-gray-100" />
+            <button className="h-12 flex items-center rounded-lg px-4 transition-colors bg-blue-700 bg-opacity-10 text-blue-900 text-sm font-semibold hover:bg-blue-900 hover:text-white">
+              <FaFacebook size={26} className="justify-self-start" />
+              <span className="mx-auto">Facebook ile devam et</span>
+            </button>
           </div>
         </div>
       </div>
