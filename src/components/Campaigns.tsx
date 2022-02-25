927d463684750899229a739d2b98e5580a52a4c5
Ferruh <ferruhcihan20@gmail.com>
2022-02-26 01:36:41 +0300

    feat:add & update campaigns section

--- src/components/Campaigns.tsx
+++ src/components/Campaigns.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
+import Banners from "../api/banners.json";
+import Title from "components/ui/Title";
 
 const Campaigns = () => {
-  return <div>Campaigns</div>;
+  const [banners, setBanners] = useState([]) as any;
+
+  useEffect(() => {
+    setBanners(Banners);
+  }, []);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  };
+
+  return (
+    <div className="container mx-auto py-8">
+      <Title>Kampanyalar</Title>
+      <Slider {...settings}>
+        {banners?.map((banner: any, index: number) => (
+          <div key={index}>
+            <img src={banner.image} alt={`banner-${banner.id}`} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 };
 
 export default Campaigns;
