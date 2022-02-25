10822e4453555a8a5952246e60e5bdac3439d987
Ferruh <ferruhcihan20@gmail.com>
2022-02-26 01:15:45 +0300

    feat:update category comp & complete categories

--- src/components/ui/Category.tsx
+++ src/components/ui/Category.tsx
@@ -6,9 +6,18 @@ interface ICategory {
 
 const Category = ({ category: { id, title, image } }: ICategory) => {
   return (
-    <a href="#" className="flex flex-col items-center text-center p-4">
-      <img src={image} alt={title} />
-      <span>{title}</span>
+    <a
+      href="#"
+      className="flex flex-col group items-center gap-y-2 text-center p-4 transition hover:bg-purple-50"
+    >
+      <img
+        src={image}
+        alt={title}
+        className="w-12 h-12 rounded border border-gray-200"
+      />
+      <span className="text-sm font-semibold text-gray-700 group-hover:text-brand-color tracking-tight">
+        {title}
+      </span>
     </a>
   );
 };
